71282729c35fa088b50de0567bf9f178a199e2cf
david.keeler <SeussWithMoose@gmail.com>
2019-08-03 18:00:14 -0700

    changes

--- take_pic_with_button.py
+++ take_pic_with_button.py
@@ -4,10 +4,12 @@ import datetime
 from PIL import Image
 import time
 
-PIC_DIR = '/tmp/'
+PIC_DIR = ''
 
 class KingOfTheHill:
     def __init__(self, input_pin):
+        self.input_pin = input_pin
+        
         # set up cammera
         self.camera = PiCamera()
         self.camera.start_preview()
@@ -15,11 +17,11 @@ class KingOfTheHill:
         # set up button
         GPIO.setmode(GPIO.BOARD) # Use physical pin numbering
         GPIO.setup(input_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(input_pin, GPIO.RISING, callback=button_callback)
+        #GPIO.add_event_detect(input_pin, GPIO.RISING, callback=self.button_callback)
 
         self.last_layer = None
 
-    def button_callback(self, channel):
+    def picture(self):
         pic_file = PIC_DIR + "probably_someone_getting_hurt_{}.png".format(datetime.datetime.now())
         self.camera.capture(pic_file)
         self.display_image(pic_file)
@@ -32,19 +34,27 @@ class KingOfTheHill:
             ))
         # Paste the original image into the padded one
         pad.paste(img, (0, 0))
-
+            
         o = self.camera.add_overlay(pad.tobytes(), size=img.size)
+        if self.last_layer:
+            self.last_layer.layer = 2
         o.alpha = 255
         o.layer = 3
-        self.camera.preview.alpha = 0
+        
         if self.last_layer:
             self.camera.remove_overlay(self.last_layer)
         self.last_layer = o
+
+        time.sleep(5)
+
+    def button_pressed(self):
+        return GPIO.input(self.input_pin) == GPIO.HIGH
+        
 
 if __name__ == '__main__':
     hill = KingOfTheHill(10)
 
     while True:
-        # run until someone kills me
-
+        if hill.button_pressed():
+                hill.picture()
     GPIO.cleanup()
